c5a031de04de7b2883bcacdf50af54e0a1a527fb
EVZYH <39973978+EVZYH@users.noreply.github.com>
2024-10-13 11:09:09 +0800

    update to 23.05.4

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # immortalwrt-ci  
 > 自用的路由器openwrt固件  
- - 所有固件均使用[immortalwrt](https://github.com/immortalwrt/immortalwrt)的openwrt-23.05分支，tag为v23.05.3  
+ - 所有固件均使用[immortalwrt](https://github.com/immortalwrt/immortalwrt)的openwrt-23.05分支，tag为v23.05.4  
  - 默认管理地址为`192.168.2.1`，密码为空  
   
 # 由[此项目](https://github.com/ibook86/newifi3-d2-openwrt)修改而来  
--- build-r1.sh
+++ build-r1.sh
@@ -7,7 +7,7 @@ export OP_BUILD_PATH=$PWD
 git clone -b openwrt-23.05 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt
 
 cd "${OP_BUILD_PATH}"/immortalwrt || exit
-git checkout v23.05.3
+git checkout v23.05.4
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
 mv "${OP_BUILD_PATH}"/r1.config "${OP_BUILD_PATH}"/immortalwrt/.config
