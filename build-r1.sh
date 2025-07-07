4ea208c290428d655654d49b218005e0e8c49480
EVZYH <39973978+EVZYH@users.noreply.github.com>
2025-07-07 11:46:30 +0800

    update to 24.10.2

--- build-r1.sh
+++ build-r1.sh
@@ -7,7 +7,7 @@ export OP_BUILD_PATH=$PWD
 git clone -b openwrt-24.10 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt
 
 cd "${OP_BUILD_PATH}"/immortalwrt || exit
-git checkout v24.10.1
+git checkout v24.10.2
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
 mv "${OP_BUILD_PATH}"/r1.config "${OP_BUILD_PATH}"/immortalwrt/.config
