ace7f81622608dcac1ba08a1efb54f934be90863
EVZYH <39973978+EVZYH@users.noreply.github.com>
2026-05-27 18:50:50 +0800

    update to v25.12.0

--- build-jcg-q30-pro.sh
+++ build-jcg-q30-pro.sh
@@ -4,10 +4,10 @@ sudo apt full-upgrade -y
 sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib g++-multilib git gnutls-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev libreadline-dev libssl-dev libtool libyaml-dev libz-dev lld llvm lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip python3-ply python3-docutils python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev zstd
 
 export OP_BUILD_PATH=$PWD
-git clone -b openwrt-24.10 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt
+git clone -b openwrt-25.12 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt
 
 cd "${OP_BUILD_PATH}"/immortalwrt || exit
-git checkout v24.10.4
+git checkout v25.12.0
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
 mv "${OP_BUILD_PATH}"/jcg-q30-pro.config "${OP_BUILD_PATH}"/immortalwrt/.config
